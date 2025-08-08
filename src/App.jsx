f6ae8b0035ee3034e65ed15fdcf17968b4567552
quietjun802 <whdydwns802@gmail.com>
2025-08-08 11:40:51 +0900

    1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Timer from './components/Timer'
+import { useState } from 'react'
+function App() {
+
+
+  return (
+    <div className='app-container'>
+      <Timer />
+    </div>
+  )
+}
+
+export default App
